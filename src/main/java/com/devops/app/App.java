4db8865e92ca7fb0cf7f6b9fe4f3bc660da0f4dc
VMTrooper <VMTrooper@gmail.com>
2014-06-16 20:43:56 -0700

    Update App.java
    Reset the output message.

--- src/main/java/com/devops/app/App.java
+++ src/main/java/com/devops/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello DevOps World!  Here's a change!" );
+        System.out.println( "Hello DevOps World!" );
     }
 }
